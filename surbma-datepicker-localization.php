4f99e1e6d3f0c080dbf262111efc77701cbdcf3e
Surbma <peter@surbma.hu>
2013-09-21 17:56:05 +0200

    1.0.0 Datepicker localization

---
+++ surbma-datepicker-localization.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+Plugin Name: Surbma - Datepicker fordítás Gravity Forms űrlapokhoz
+Plugin URI: http://premiumwp.hu/
+Description: Ezzel a bővítménnyel a datepicker dátum mező a weboldal beállításaiban megadott nyelven jelenik meg a Gravity Forms űrlapoknál.
+Version: 1.0.0
+Author: Surbma
+Author URI: http://surbma.hu/
+License: GPL2
+*/
+
+function surbma_enqueue_datepicker_script( $form, $is_ajax ) {
+	wp_enqueue_script( 'surbma-datepicker-local', plugins_url( '', __FILE__ ) . '/languages/jquery.ui.datepicker-' . get_bloginfo('language') . '.js' );
+}
+add_action( 'gform_enqueue_scripts', 'surbma_enqueue_datepicker_script', 10, 2 );
+
